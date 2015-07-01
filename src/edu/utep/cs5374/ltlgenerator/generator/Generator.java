77021a280af7a08a80643997335bc8610d11086f
rpgmaster280 <rpgmaster280@gmail.com>
2015-06-30 21:12:01 -0500

    Initial Version
    -Robert M

---
+++ src/edu/utep/cs5374/ltlgenerator/generator/Generator.java
@@ -0,0 +1,5 @@
+package edu.utep.cs5374.ltlgenerator.generator;
+
+public class Generator {
+
+}
---
+++ src/edu/utep/cs5374/ltlgenerator/generator/Main.java
@@ -0,0 +1,8 @@
+package edu.utep.cs5374.ltlgenerator.generator;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, world!");
+	}
+}
