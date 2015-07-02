a9ca5ae4f5277e40afdbfb7782a878f609fa1c92
Porag <poragbuet@gmail.com>
2015-07-02 12:49:32 -0600

    modified variable name

--- src/edu/utep/cs5374/ltlgenerator/cp/ConsecutiveC.java
+++ src/edu/utep/cs5374/ltlgenerator/cp/ConsecutiveC.java
@@ -7,26 +7,26 @@ public class ConsecutiveC implements CompositePropositionParent {
 	private static final String CLOSE_Parenth = ")";
 	private static final String NEXT = "X";
 	
-	public String ltlFormulaGenerator(int numP, int countRecursion, StringBuilder str){
+	public String ltlFormulaGenerator(int numP, int countRecursion, StringBuilder ltlFormula){
 		if(numP == countRecursion)
 		{
-			str.append("p");
+			ltlFormula.append("p");
 			// appending (countRecursion - 1) 
 			// as we are considering p0 instead of p1 when number of prop is 1
-			return str.append(countRecursion-1).toString(); 
+			return ltlFormula.append(countRecursion-1).toString(); 
 		}
 		else {
-			str.append("p");
+			ltlFormula.append("p");
 			// appending (countRecursion - 1) 
 			// as we are considering p0 instead of p1 when number of prop is 1
-			str.append(countRecursion-1);
-			str.append(AND);
-			str.append(NEXT);
-			str.append(OPEN_Parenth);
+			ltlFormula.append(countRecursion-1);
+			ltlFormula.append(AND);
+			ltlFormula.append(NEXT);
+			ltlFormula.append(OPEN_Parenth);
 			countRecursion++;
-			ltlFormulaGenerator(numP, countRecursion, str);
-			str.append(CLOSE_Parenth);
-			return str.toString();
+			ltlFormulaGenerator(numP, countRecursion, ltlFormula);
+			ltlFormula.append(CLOSE_Parenth);
+			return ltlFormula.toString();
 		}
 	}
 	
