4273721c1d50dde3c88401517a1142994bedf5eb
himapriya777 <himapriya777@gmail.com>
2015-07-02 00:30:41 -0600

    updated AtLeastOneH

--- src/edu/utep/cs5374/ltlgenerator/cp/AtleastOneH.java
+++ src/edu/utep/cs5374/ltlgenerator/cp/AtleastOneH.java
@@ -2,10 +2,30 @@ package edu.utep.cs5374.ltlgenerator.cp;
 
 public class AtleastOneH extends CompositePropositionParent {
 
+private static final String OR = " | ";
+	
 	public static String compute(int aCount)
 	{
-		//AtleastOneH and AtleastOneC are identical.
-		return AtleastOneC.compute(aCount);
+		if (aCount <= 0)
+		{
+			return "";
+		}
+		
+		StringBuilder stringBuilder = new StringBuilder("(");
+		
+		for(int i=0;i< aCount;i++)
+		{
+			stringBuilder.append(" p" + i + " ");
+			
+			if(i < aCount - 1)
+			{
+				stringBuilder.append(OR);
+			}
+		}
+		
+		stringBuilder.append(")");
+		
+		return stringBuilder.toString();
 	}
 
 }
