77a917037c12a4b6e71fa6de39a858147cbcf180
jvasquez9 <jvasquez9@miners.utep.edu>
2015-07-01 21:41:02 -0600

    ParallelE
    Works !

--- src/edu/utep/cs5374/ltlgenerator/cp/ParallelE.java
+++ src/edu/utep/cs5374/ltlgenerator/cp/ParallelE.java
@@ -9,11 +9,67 @@ package edu.utep.cs5374.ltlgenerator.cp;
  */
 public class ParallelE extends CompositePropositionParent {
 
-	/**
-	 * 
-	 */
-	public ParallelE() {
-		// TODO Auto-generated constructor stub
-	}
+	public class ParallelE  {
 
-}
+		private static final String OR = " | ";
+		private static final String AND = "& ";
+		private static final String NOT = "!";
+		private static final String UNTIL = "U";
+		private static final String OPEN_P = "(";
+		private static final String CLOSE_P = ")";
+
+
+		public static String compute(int aCount)
+		{
+			if (aCount <= 0)
+			{
+				return "";
+			}
+
+			/*First half of ParallelE*/
+			StringBuilder stringBuilder = new StringBuilder(OPEN_P);
+			for(int i=0;i< aCount;i++)
+			{
+				stringBuilder.append(NOT + " p" + i + " ");
+				if(i < aCount - 1)
+				{
+					stringBuilder.append(AND);
+				}
+
+			}
+			stringBuilder.append(CLOSE_P);
+			/*First half done*/
+
+
+			/**Second half of ParallelE**/
+			stringBuilder.append(AND);
+			stringBuilder.append(OPEN_P);
+			stringBuilder.append(OPEN_P);
+			for(int i = 0; i < aCount; i++){
+				stringBuilder.append(NOT + " p" + i + " ");
+				if(i < aCount - 1)
+				{
+					stringBuilder.append(AND);
+				}
+			}
+			stringBuilder.append(CLOSE_P);
+			/*Second half done*/
+
+
+			/*Third part of parallelE*/
+			stringBuilder.append(UNTIL);
+			stringBuilder.append(OPEN_P);
+			for(int i = 0; i < aCount; i++){
+				stringBuilder.append(" p" + i + " ");
+				if(i < aCount - 1)
+				{
+					stringBuilder.append(AND);
+				}
+			}
+			stringBuilder.append(CLOSE_P);
+			stringBuilder.append(CLOSE_P);
+			/*Third part done*/
+
+			return stringBuilder.toString();
+		}
+	}
