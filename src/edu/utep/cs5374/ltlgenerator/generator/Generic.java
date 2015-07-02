f3e1404fb2c292672f1c87b86860c5d1fb9e099e
himapriya777 <himapriya777@gmail.com>
2015-07-01 20:26:55 -0600

    Generic for other three

--- src/edu/utep/cs5374/ltlgenerator/generator/Generic.java
+++ src/edu/utep/cs5374/ltlgenerator/generator/Generic.java
@@ -1,4 +1,5 @@
 package edu.utep.cs5374.ltlgenerator.generator;
+
 import java.util.Scanner;
 
 
@@ -22,9 +23,17 @@ public class Generic {
 		System.out.println("Enter the number of propositions");
 		n=sc.nextInt();
 		String[] LProp = new String[n];
-		for(int i=0;i<LProp.length;i++)
+		String[] PProp = new String[n];
+		String[] QProp = new String[n];
+		String[] RProp = new String[n];
+		for(int i=0;i<n;i++)
 		{
 			LProp[i]="l"+(i+1);
+			PProp[i]="p"+(i+1);
+			QProp[i]="q"+(i+1);
+			RProp[i]="r"+(i+1);
+
+			
 			//System.out.println(LProp[i]);
 		}
 		
