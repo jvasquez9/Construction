c727959bdfd35298eb8fa44d571f8c8f1388578e
Porag <poragbuet@gmail.com>
2015-07-02 00:33:49 -0600

    working on consecutive c

--- src/edu/utep/cs5374/ltlgenerator/cp/ConsecutiveC.java
+++ src/edu/utep/cs5374/ltlgenerator/cp/ConsecutiveC.java
@@ -1,9 +1,22 @@
 package edu.utep.cs5374.ltlgenerator.cp;
 
+/**
+ * @author Porag
+ *
+ */
+
 public class ConsecutiveC extends CompositePropositionParent {
 
-	public ConsecutiveC() {
-		// TODO Auto-generated constructor stub
+	private static final String OR = " | ";
+	private static final String OPEN_P = "(";
+	private static final String CLOSE_P = ")";
+
+	public static String compute(int aCount)
+	{
+		StringBuilder stringBuilder = new StringBuilder(OPEN_P);
+		
+		stringBuilder.append(CLOSE_P);
+		return stringBuilder.toString();
 	}
 
 }
