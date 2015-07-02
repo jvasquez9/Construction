79e15e1d09479cb865b05764472e0024cf392386
himapriya777 <himapriya777@gmail.com>
2015-07-01 22:12:51 -0600

    added ParallelH

--- src/edu/utep/cs5374/ltlgenerator/cp/ParallelH.java
+++ src/edu/utep/cs5374/ltlgenerator/cp/ParallelH.java
@@ -2,8 +2,29 @@ package edu.utep.cs5374.ltlgenerator.cp;
 
 public class ParallelH extends CompositePropositionParent {
 
-	public ParallelH() {
-		// TODO Auto-generated constructor stub
+	private static final String AND = " & ";
+	
+	public static String compute(int aCount)
+	{
+		if (aCount <= 0)
+		{
+			return "";
+		}
+		
+		StringBuilder stringBuilder = new StringBuilder("(");
+		
+		for(int i=0;i< aCount;i++)
+		{
+			stringBuilder.append(" p" + i + " ");
+			
+			if(i < aCount - 1)
+			{
+				stringBuilder.append(AND);
+			}
+		}
+		
+		stringBuilder.append(")");
+		
+		return stringBuilder.toString();
 	}
-
 }
