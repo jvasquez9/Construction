77fb7b3d0f5e7253e860498d7b074875025d02f1
himapriya777 <himapriya777@gmail.com>
2015-07-01 21:28:08 -0600

    Added input format 1 to 12

--- src/edu/utep/cs5374/ltlgenerator/generator/Main.java
+++ src/edu/utep/cs5374/ltlgenerator/generator/Main.java
@@ -1,8 +1,56 @@
 package edu.utep.cs5374.ltlgenerator.generator;
 
+import java.util.Scanner;
+
+import edu.utep.cs5374.ltlgenerator.cp.AtleastOneC;
+import edu.utep.cs5374.ltlgenerator.cp.AtleastOneH;
+import edu.utep.cs5374.ltlgenerator.cp.ParallelC;
+
+
 public class Main {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello, world!");
+	public static void main(String[] args) {
+
+			int l, p, q, r, n; // to store user input type of proposition
+			String L="", P="", Q="", R=""; //to keep pattern before ANDL, ANDR
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println(
+					"1 # AtleastOneC "+"\n"+ 
+					"2 # AtleastOneH "+"\n"+ 
+					"3 # AtleastOneE "+"\n"+ 
+					"4 # ParallelC"+"\n"+
+					"5 # ParallelH"+"\n"+
+					"6 # ParallelE"+"\n"+		
+					"7 # ConsecutiveC" +"\n"+
+					"8 # ConsecutiveH" +"\n"+
+					"9 # ConsecutiveE" +"\n"+		
+					"10 # EventualC"+"\n"+
+					"11 # EventualH"+"\n"+
+			        "12 # EventualE");
+			
+			System.out.println("Enter the number of propositions:");
+			n=sc.nextInt();
+			
+			System.out.println("Enter the type of proposition for L:");
+			l=sc.nextInt();
+			
+			if(l==1)
+			{
+				L= AtleastOneC.compute(n);
+				System.out.println("AtleastOneC pattern is" + L);
+
+			} 
+			if(l==2)
+			{
+				L= AtleastOneH.compute(n);
+				System.out.println("ParallelC pattern is"+L);
+			} 
+			if(l==4)
+			{
+				L= ParallelC.compute(n);
+				System.out.println("ParallelC pattern is"+L);
+			} 
+			sc.close();
+		}
 	}
-}
