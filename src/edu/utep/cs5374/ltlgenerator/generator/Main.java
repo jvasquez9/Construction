6a14e82fa625ffc4b5cff64ca938a561aaf79cb9
rpgmaster280 <rpgmaster280@gmail.com>
2015-07-01 22:13:02 -0500

    -Removed Generic.java since it's logic is now in Main.java -Did a cleanup of Main.java

--- src/edu/utep/cs5374/ltlgenerator/generator/Main.java
+++ src/edu/utep/cs5374/ltlgenerator/generator/Main.java
@@ -8,11 +8,12 @@ import edu.utep.cs5374.ltlgenerator.cp.ParallelC;
 
 public class Main {
 	public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			int l,p,q,r,n; // to store user input type of proposition
-			String L="",P="",Q="",R=""; //to keep pattern before ANDL, ANDR
+
+			int l, p, q, r, n; // to store user input type of proposition
+			String L="", P="", Q="", R=""; //to keep pattern before ANDL, ANDR
 			
 			Scanner sc = new Scanner(System.in);
+			
 			System.out.println(
 			"1 # AtleastOneC "+"\n"+ 
 			"2 # ParallelC"+"\n"+
@@ -22,33 +23,25 @@ public class Main {
 		    "6 # ParallelE"+"\n"+
 		    "7 # ConsecutiveE"+"\n"+
 		    "8 # EventualE");
-			System.out.println("Enter the number of propositions");
-			n=sc.nextInt();
-			String[] LProp = new String[n];
-			for(int i=0;i<LProp.length;i++)
-			{
-				LProp[i]="l"+(i+1);
-				//System.out.println(LProp[i]);
-			}
 			
-			System.out.println("Enter the type of proposition for L");
+			System.out.println("Enter the number of propositions:");
+			n=sc.nextInt();
+			
+			System.out.println("Enter the type of proposition for L:");
 			l=sc.nextInt();
 			
 			if(l==1)
 			{
-				AtleastOneC aoCP = new AtleastOneC();
-				L=aoCP.compute(n);
-				System.out.println("AtleastOneC pattern is"+L);
+				L= AtleastOneC.compute(n);
+				System.out.println("AtleastOneC pattern is" + L);
 
 			} 
 			if(l==2)
 			{
-				ParallelC plCP = new ParallelC();
-				L=plCP.compute(n);
+				L= ParallelC.compute(n);
 				System.out.println("ParallelC pattern is"+L);
-
-				
 			} 
-
+			
+			sc.close();
 		}
 	}
