5f46bd1b1d695efb8ccd4e546d6d281d7fc1a612
jvasquez9 <jvasquez9@miners.utep.edu>
2015-07-02 18:53:36 -0600

    Fixed a missing P
    Added a missing P

--- src/edu/utep/cs5374/ltlgenerator/cp/ConsecutiveH.java
+++ src/edu/utep/cs5374/ltlgenerator/cp/ConsecutiveH.java
@@ -31,7 +31,7 @@ public class ConsecutiveH  implements CompositePropositionParent{
 			return ltlFormula.toString();
 		}
 		else{
-			return ltlFormula.toString();
+			return ltlFormula.append(")").toString();
 		}
 	}
 
