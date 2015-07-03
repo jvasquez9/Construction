5d05cfec26e1fac414253ade767d1bc8249ba460
rpgmaster280 <rpgmaster280@gmail.com>
2015-07-02 22:38:21 -0500

    -AtleastOneH is the exact same as AtleastOneC so I delegated the responsibility. Please quit reimplementing this class.

--- src/edu/utep/cs5374/ltlgenerator/cp/AtleastOneH.java
+++ src/edu/utep/cs5374/ltlgenerator/cp/AtleastOneH.java
@@ -1,34 +1,11 @@
 package edu.utep.cs5374.ltlgenerator.cp;
 
 public class AtleastOneH implements CompositePropositionParent {
-
-	private static final String OR = " | ";
-	private static final String OPEN_Parenth = "(";
-	private static final String CLOSE_Parenth = ")";
 	
 	@Override
 	public String compute(int numProposition)
 	{
-		if (numProposition <= 0)
-		{
-			return "";
-		}
-		
-		StringBuilder ltlFormula = new StringBuilder(OPEN_Parenth);
-		
-		for(int i=0;i< numProposition;i++)
-		{
-			ltlFormula.append("p" + i + " ");
-			
-			if(i < numProposition - 1)
-			{
-				ltlFormula.append(OR);
-			}
-		}
-		
-		ltlFormula.append(CLOSE_Parenth);
-		
-		return ltlFormula.toString();
+		return new AtleastOneC().compute(numProposition);
 	}
 
 }
