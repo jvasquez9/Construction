e61c3505ba1647ad24d598644987e8d239f31da3
fjibarbo <fjibarbo@miners.utep.edu>
2015-07-03 19:34:04 -0600

    removed parts of the generator that weren't working

--- src/edu/utep/cs5374/ltlgenerator/cp/ConsecutiveE.java
+++ src/edu/utep/cs5374/ltlgenerator/cp/ConsecutiveE.java
@@ -14,7 +14,6 @@ public class ConsecutiveE implements CompositePropositionParent {
 	@Override
 	public String compute(int aCount) 
 	{
-		int count = 0;
 		if (aCount <= 0)
 		{
 			return "";
@@ -61,36 +60,10 @@ public class ConsecutiveE implements CompositePropositionParent {
 			for(;nextP < aCount; nextP++){
 				stringBuilder.append(NOT + "p" + nextP + " " + AND);
 			}
-		
+		}
 		stringBuilder.append(NEXT);
 		stringBuilder.append(OPEN_P);
 		
-		nextP = i + 1;
-		if(aCount - i != 2){
-			for(;nextP < aCount; nextP++){
-				if(aCount - nextP == 1){
-					stringBuilder.append(NOT + "p" + nextP);
-				}
-				else{
-					stringBuilder.append(NOT + "p" + nextP + " " + AND);
-				}
-			}
-				stringBuilder.append(CLOSE_P + " " + NEXT + " " + OPEN_P);
-				count += 2;
-		}
-		else{
-			stringBuilder.append(NOT + "p" + nextP + " "
-			+ NEXT + " " + "p" + nextP);
-			count += 2;
-			break;
-		}
-	}
-	
-	while(count > 0)
-	{
-		stringBuilder.append(CLOSE_P);
-		count--;
-	}
 		
 		return stringBuilder.toString();
 		
