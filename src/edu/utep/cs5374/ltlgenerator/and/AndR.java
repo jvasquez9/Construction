f5f63f34083697bc620551f0e00ad552cfd1fcf8
rpgmaster280 <rpgmaster280@gmail.com>
2015-07-12 12:46:52 -0500

    -Added initial version of and subsystem

---
+++ src/edu/utep/cs5374/ltlgenerator/and/AndR.java
@@ -0,0 +1,11 @@
+package edu.utep.cs5374.ltlgenerator.and;
+
+public class AndR implements AndParent{
+
+	@Override
+	public String and(String leftHandSide, String rightHandSide) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/edu/utep/cs5374/ltlgenerator/and/AndUnitTests.java
@@ -0,0 +1,14 @@
+package edu.utep.cs5374.ltlgenerator.and;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AndUnitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
