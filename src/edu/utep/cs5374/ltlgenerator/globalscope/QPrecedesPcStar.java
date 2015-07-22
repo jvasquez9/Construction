ef4d4c7ac38fc542dc16f1966c28dba4252676ac
jvasquez9 <jvasquez9@miners.utep.edu>
2015-07-22 17:51:53 -0600

    Q Preceds Pc Star
    Added a VERY rough method that just returns a string. That's it

---
+++ src/edu/utep/cs5374/ltlgenerator/globalscope/QPrecedesPcStar.java
@@ -0,0 +1,19 @@
+package edu.utep.cs5374.ltlgenerator.globalscope;
+
+import edu.utep.cs5374.ltlgenerator.symbols.Symbols;
+
+public class QPrecedesPcStar extends GlobalScopeParent{
+
+	@Override
+	public String getFormula(String Q, int numberofprop) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String formulaOfScope(String Pltl, String Qltl){
+		return Symbols.NOT + Symbols.OPEN_Parenth + Symbols.OPEN_Parenth + Symbols.AND + Qltl + Symbols.CLOSE_Parenth +
+				Symbols.UNTIL + Symbols.OPEN_Parenth + Pltl + Symbols.AND + Symbols.NOT + Qltl + Symbols.CLOSE_Parenth + Symbols.CLOSE_Parenth;
+	}
+	
+
+}
