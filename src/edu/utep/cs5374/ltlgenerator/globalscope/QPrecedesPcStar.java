13fadf69f6b2b28a1065e7067dac7f9b9258a362
jvasquez9 <jvasquez9@miners.utep.edu>
2015-07-22 17:58:04 -0600

    Comment and Fix
    Fixed the way the string looks and added a comment

--- src/edu/utep/cs5374/ltlgenerator/globalscope/QPrecedesPcStar.java
+++ src/edu/utep/cs5374/ltlgenerator/globalscope/QPrecedesPcStar.java
@@ -10,7 +10,8 @@ public class QPrecedesPcStar extends GlobalScopeParent{
 		return null;
 	}
 
-	public String formulaOfScope(String Pltl, String Qltl){
+	public String formulaOfScope(String P_ltl, String Q_ltl){
+		//      ! ( ( ! Q_ltl ) U ( P_ltl ) ) U P_ltl)
 		return Symbols.NOT + Symbols.OPEN_Parenth + Symbols.OPEN_Parenth + Symbols.AND + Qltl + Symbols.CLOSE_Parenth +
 				Symbols.UNTIL + Symbols.OPEN_Parenth + Pltl + Symbols.AND + Symbols.NOT + Qltl + Symbols.CLOSE_Parenth + Symbols.CLOSE_Parenth;
 	}
