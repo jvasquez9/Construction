030b6e4665f41f259797526c308f15cb01ab66ae
himapriya777 <himapriya777@gmail.com>
2015-07-27 16:47:53 -0600

    QRespondstoPBeforeRc and QRespondstoPBeforeRe added

---
+++ src/edu/utep/cs5374/ltlgenerator/beforerscope/QRespondstoPBeforeRc.java
@@ -0,0 +1,30 @@
+package edu.utep.cs5374.ltlgenerator.beforerscope;
+
+import edu.utep.cs5374.ltlgenerator.and.AndL;
+import edu.utep.cs5374.ltlgenerator.and.AndR;
+import edu.utep.cs5374.ltlgenerator.symbols.Symbols;
+
+public class QRespondstoPBeforeRc extends BeforeRScopeParent {
+
+	@Override
+	public String getFormula(String P_ltl, String Q_ltl, String R_ltl,int numProposition) {
+		// ¬((¬ RLTL) U ((PLTL & r ¬ RLTL) & l((¬ (QLTL & r ¬ RLTL)) U RLTL)))
+		// need to cross check parantheses
+		
+	
+		
+		String andRLeft=new AndR().and(P_ltl, Symbols.NOT+R_ltl);
+		String andRRight=new AndR().and(Q_ltl, Symbols.NOT+R_ltl);
+		
+		String andL=new AndL().and(Symbols.OPEN_Parenth+andRLeft+Symbols.CLOSE_Parenth,				
+				Symbols.OPEN_Parenth+Symbols.OPEN_Parenth+Symbols.NOT+Symbols.OPEN_Parenth+andRRight
+				+Symbols.CLOSE_Parenth+Symbols.CLOSE_Parenth+Symbols.UNTIL+R_ltl+Symbols.CLOSE_Parenth);
+		
+		
+		return Symbols.NOT+Symbols.OPEN_Parenth+Symbols.OPEN_Parenth+Symbols.NOT+R_ltl+Symbols.CLOSE_Parenth+
+				Symbols.UNTIL+Symbols.OPEN_Parenth+andL+Symbols.CLOSE_Parenth+Symbols.CLOSE_Parenth;
+		
+		
+	}
+
+}
---
+++ src/edu/utep/cs5374/ltlgenerator/beforerscope/QRespondstoPBeforeRe.java
@@ -0,0 +1,12 @@
+package edu.utep.cs5374.ltlgenerator.beforerscope;
+
+public class QRespondstoPBeforeRe extends BeforeRScopeParent {
+
+	@Override
+	public String getFormula(String P_ltl, String Q_ltl, String R_ltl,
+			int numProposition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
