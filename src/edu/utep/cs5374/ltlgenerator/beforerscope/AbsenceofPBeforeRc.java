c8914354a7182d6b2eb40d718bae15e086f79a2d
fjibarbo <fjibarbo@miners.utep.edu>
2015-07-26 23:51:25 -0600

    Adjusted classes

--- src/edu/utep/cs5374/ltlgenerator/beforerscope/AbsenceofPBeforeRc.java
+++ src/edu/utep/cs5374/ltlgenerator/beforerscope/AbsenceofPBeforeRc.java
@@ -1,5 +1,8 @@
 package edu.utep.cs5374.ltlgenerator.beforerscope;
 
-public class AbsenceofPBeforeRc {
-
+public class AbsenceofPBeforeRc extends BeforeRScopeParent{
+	public String getFormula(String P_ltl, String Q_ltl, String R_ltl,
+			int numProposition) {
+		return null;
+	}
 }
--- src/edu/utep/cs5374/ltlgenerator/beforerscope/AbsenceofPBeforeRe.java
+++ src/edu/utep/cs5374/ltlgenerator/beforerscope/AbsenceofPBeforeRe.java
@@ -1,5 +1,8 @@
 package edu.utep.cs5374.ltlgenerator.beforerscope;
 
-public class AbsenceofPBeforeRe {
-
+public class AbsenceofPBeforeRe extends BeforeRScopeParent{
+	public String getFormula(String P_ltl, String Q_ltl, String R_ltl,
+			int numProposition) {
+		return null;
+	}
 }
