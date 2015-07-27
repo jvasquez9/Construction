93099980116c2fc2624e73523a1d39f737a1b1a9
fjibarbo <fjibarbo@miners.utep.edu>
2015-07-26 23:06:44 -0600

    Added AbsenceofPBeforeRc, AbsenceofPBeforeRe, and ExistenceofPBeforeRc
    Classes are not functional, just stem classes to get a better
    understanding of the system.

---
+++ src/edu/utep/cs5374/ltlgenerator/beforerscope/ExistenceofPBeforeRc.java
@@ -0,0 +1,8 @@
+package edu.utep.cs5374.ltlgenerator.beforerscope;
+
+public class ExistenceofPBeforeRc extends BeforeRScopeParent{
+	public String getFormula(String P_ltl, String Q_ltl, String R_ltl,
+			int numProposition) {
+		return null;
+	}
+}
