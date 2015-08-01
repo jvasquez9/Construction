0a67f7ba74fee6c782f47ac164f4d6452c6d9a36
jvasquez9 <jvasquez9@miners.utep.edu>
2015-07-31 18:51:22 -0600

    Remaining scopes parent
    uploading this now

---
+++ src/RemainingScopes/RemainingScopesParent.java
@@ -0,0 +1,6 @@
+package RemainingScopes;
+
+public abstract class RemainingScopesParent {
+	
+	public abstract String getFormula(String P_ltl, String Q_ltl, String R_ltl, String L_ltl, int numProposition);
+}
