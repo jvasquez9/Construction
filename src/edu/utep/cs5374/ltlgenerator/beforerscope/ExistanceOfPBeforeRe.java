a88fbf503fd73087bf6a1d6ff3d84185e9eedecd
jvasquez9 <jvasquez9@miners.utep.edu>
2015-08-01 14:00:35 -0600

    Existance of P before Re
    finishing last edits

--- src/edu/utep/cs5374/ltlgenerator/beforerscope/ExistanceOfPBeforeRe.java
+++ src/edu/utep/cs5374/ltlgenerator/beforerscope/ExistanceOfPBeforeRe.java
@@ -1,11 +1,37 @@
 package edu.utep.cs5374.ltlgenerator.beforerscope;
 
+import edu.utep.cs5374.ltlgenerator.and.AndMinusL;
+import edu.utep.cs5374.ltlgenerator.and.AndR;
+import edu.utep.cs5374.ltlgenerator.cp.ParallelH;
+import edu.utep.cs5374.ltlgenerator.symbols.Symbols;
+
 public class ExistanceOfPBeforeRe extends BeforeRScopeParent{
 
 	@Override
 	public String getFormula(String P_ltl, String Q_ltl, String R_ltl, int numProposition) {
 		// (♦RLTL)→	((¬((¬r1 ∧···∧¬rn)∧X(RLTLH ))) U (PLTL&r¬RLTLH ))
-		return null;
+		
+		String firstSyllable = Symbols.OPEN_Parenth + Symbols.F + R_ltl + Symbols.CLOSE_Parenth + Symbols.RIGHT_ARROW + Symbols.OPEN_Parenth + Symbols.OPEN_Parenth +
+				Symbols.NOT + Symbols.OPEN_Parenth + Symbols.OPEN_Parenth;
+		
+		String Rh_ltl = new ParallelH().compute(numProposition, 'r');
+		String andRPart = new AndR().and(P_ltl, Symbols.NOT + Rh_ltl);
+		String thirdSyllable = Symbols.AND + Symbols.NEXT + Symbols.OPEN_Parenth + R_ltl +  Symbols.CLOSE_Parenth + Symbols.CLOSE_Parenth + Symbols.CLOSE_Parenth +	Symbols.UNTIL; 
+		
+		String fourthSyllable = Symbols.OPEN_Parenth + andRPart + Symbols.CLOSE_Parenth + Symbols.CLOSE_Parenth ; 
+		
+		String partialPart = "";
+		for(int i = 0; i < numProposition; i++)
+		{
+			if(i == numProposition-1)
+				partialPart += "" + Symbols.NOT + "r" + i;
+			else
+				partialPart += "" + Symbols.NOT + "r" + i + Symbols.AND; 
+		}
+		
+		
+		return firstSyllable + partialPart + thirdSyllable + fourthSyllable;
+		
 	}
 
 }
