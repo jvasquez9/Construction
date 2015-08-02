df73621234f95799e8ac92c33defbb2d45625af8
fjibarbo <fjibarbo@miners.utep.edu>
2015-08-02 00:51:11 -0600

    Updated content for AbsenceofPBeforeRc and AbsenceofPBeforeRe

--- src/edu/utep/cs5374/ltlgenerator/beforerscope/AbsenceofPBeforeRc.java
+++ src/edu/utep/cs5374/ltlgenerator/beforerscope/AbsenceofPBeforeRc.java
@@ -1,8 +1,24 @@
 package edu.utep.cs5374.ltlgenerator.beforerscope;
 
+import edu.utep.cs5374.ltlgenerator.and.AndL;
+import edu.utep.cs5374.ltlgenerator.and.AndR;
+import edu.utep.cs5374.ltlgenerator.symbols.Symbols;
+
 public class AbsenceofPBeforeRc extends BeforeRScopeParent{
-	public String getFormula(String P_ltl, String Q_ltl, String R_ltl,
-			int numProposition) {
-		return null;
+	public String getFormula(String P_ltl, String Q_ltl, String R_ltl,int numProposition) {
+		// �((� RLTL) U ((PLTL & r � RLTL) & l FRLTL)
+	
+		
+		String andRLeft=new AndR().and(P_ltl, Symbols.NOT+R_ltl);
+		
+		String andL=new AndL().and(Symbols.OPEN_Parenth+andRLeft+Symbols.CLOSE_Parenth,				
+				    Symbols.F+R_ltl+Symbols.CLOSE_Parenth);
+		
+		
+		return Symbols.NOT+Symbols.OPEN_Parenth+Symbols.OPEN_Parenth+Symbols.NOT+R_ltl+Symbols.CLOSE_Parenth+
+				Symbols.UNTIL+Symbols.OPEN_Parenth+andL+Symbols.CLOSE_Parenth+Symbols.CLOSE_Parenth;
+		
+		
 	}
+
 }
--- src/edu/utep/cs5374/ltlgenerator/beforerscope/AbsenceofPBeforeRe.java
+++ src/edu/utep/cs5374/ltlgenerator/beforerscope/AbsenceofPBeforeRe.java
@@ -1,8 +1,34 @@
 package edu.utep.cs5374.ltlgenerator.beforerscope;
 
+import edu.utep.cs5374.ltlgenerator.and.AndR;
+import edu.utep.cs5374.ltlgenerator.cp.ParallelH;
+import edu.utep.cs5374.ltlgenerator.symbols.Symbols;
+
 public class AbsenceofPBeforeRe extends BeforeRScopeParent{
-	public String getFormula(String P_ltl, String Q_ltl, String R_ltl,
-			int numProposition) {
-		return null;
+	@Override
+	public String getFormula(String P_ltl, String Q_ltl, String R_ltl, int numProposition) {
+		// (♦RLTL)→	((¬((¬r1 ∧···∧¬rn)∧X(RLTLH ))) U (PLTL&r¬RLTLH ))
+		
+		String firstSyllable = Symbols.OPEN_Parenth + Symbols.F + R_ltl + Symbols.CLOSE_Parenth + Symbols.RIGHT_ARROW + Symbols.OPEN_Parenth + Symbols.OPEN_Parenth +
+				Symbols.NOT + Symbols.OPEN_Parenth + Symbols.OPEN_Parenth;
+		
+		String Rh_ltl = new ParallelH().compute(numProposition, 'r');
+		String andRPart = new AndR().and(P_ltl, Symbols.NOT + Rh_ltl);
+		String thirdSyllable = Symbols.AND + Symbols.NEXT + Symbols.OPEN_Parenth + R_ltl +  Symbols.CLOSE_Parenth + Symbols.CLOSE_Parenth + Symbols.CLOSE_Parenth +	Symbols.UNTIL; 
+		
+		String fourthSyllable = Symbols.OPEN_Parenth + andRPart + Symbols.CLOSE_Parenth + Symbols.CLOSE_Parenth ; 
+		
+		String partialPart = "";
+		for(int i = 0; i < numProposition; i++)
+		{
+			if(i == numProposition-1)
+				partialPart += "" + Symbols.NOT + "r" + i;
+			else
+				partialPart += "" + Symbols.NOT + "r" + i + Symbols.AND; 
+		}
+		
+		
+		return firstSyllable + partialPart + thirdSyllable + fourthSyllable;
+		
 	}
 }
